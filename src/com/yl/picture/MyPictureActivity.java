02b949148a3cd60478898c5e6f7aa948afbf50af
466489438 <466489438@qq.com>
2015-05-07 22:57:49 +0800

    Dock

--- src/com/yl/picture/MyPictureActivity.java
+++ src/com/yl/picture/MyPictureActivity.java
@@ -1,35 +1,72 @@
 package com.yl.picture;
 
-import android.app.Activity;
+import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
+import android.view.Window;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.TabHost;
 
-public class MyPictureActivity extends Activity {
-	private Button button1;
-	private Button button2;
+public class MyPictureActivity extends TabActivity implements OnCheckedChangeListener{
+	private TabHost mTabHost;
+	private Intent mAIntent;
+	private Intent mBIntent;
+    /** Called when the activity is first created. */
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i("test", "test1");
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
-        this.button1=(Button) this.findViewById(R.id.button1);
-        this.button2=(Button) this.findViewById(R.id.button2);
-        //点击按钮进入缩略模式
-        this.button1.setOnClickListener(new View.OnClickListener() {
-			
-			public void onClick(View v) {
-				Intent intent=new Intent(MyPictureActivity.this,SuolueActivity.class);
-				startActivity(intent);
-			}
-		});
-        //点击按钮进入横铺模式
-        this.button2.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Intent intent=new Intent(MyPictureActivity.this,PingPuActivity.class);
-				startActivity(intent);
-			}
-		});
+        this.mAIntent = new Intent(MyPictureActivity.this,PingPuActivity.class);
+        this.mBIntent = new Intent(MyPictureActivity.this,SuolueActivity.class);
+		((RadioButton) findViewById(R.id.radio_button0))
+		.setOnCheckedChangeListener(this);
+		Log.i("test", "test2");
+        ((RadioButton) findViewById(R.id.radio_button1))
+		.setOnCheckedChangeListener(this);
+        Log.i("test", "test3");
+        setupIntent();
+        
     }
+
+	@Override
+	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+		if(isChecked){
+			switch (buttonView.getId()) {
+			case R.id.radio_button0:
+				Log.i("test", "test9");
+				this.mTabHost.setCurrentTabByTag("A_TAB");
+				break;
+			case R.id.radio_button1:
+				Log.i("test", "test10");
+				this.mTabHost.setCurrentTabByTag("B_TAB");
+				break;
+			
+			}
+		}
+		
+	}
+	
+	private void setupIntent() {
+		this.mTabHost = getTabHost();
+		TabHost localTabHost = this.mTabHost;
+		Log.i("test", "test5");
+		localTabHost.addTab(buildTabSpec("A_TAB", R.string.pingpu,
+				R.drawable.a1, this.mAIntent));
+		Log.i("test", "test6");
+		localTabHost.addTab(buildTabSpec("B_TAB", R.string.suolue,
+				R.drawable.a2, this.mBIntent));
+		Log.i("test", "test7");
+
+	}
+	
+	private TabHost.TabSpec buildTabSpec(String tag, int resLabel, int resIcon,
+			final Intent content) {
+		return this.mTabHost.newTabSpec(tag).setIndicator(getString(resLabel),
+				getResources().getDrawable(resIcon)).setContent(content);
+	}
 }
